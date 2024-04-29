1f2e651901f18d967977b9563661b3e84066953a
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-04-29 11:38:28 +0600

    update issue problem done

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ async function run() {
       const filter = {_id: new ObjectId(id)};
       const options = {upsert:true};
       const updateTourist = {
-        
+    
         // const updatedTouristField = {name,country,image,spotName,visitor, travelTime,seasonality,averageCost,description,location}dffdsfdsfdsf
         $set: {
           name: tourist.name,
